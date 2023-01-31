dcf1b454f99d6ea00b77f7c485ad977364be0bde
Ala Baganne <alabaganne9@gmail.com>
2023-01-31 22:30:47 +0100

    Add react router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,14 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Contact from './Contact'
+import Home from './Home';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
